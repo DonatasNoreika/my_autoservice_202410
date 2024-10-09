9b0820be8ab3d00e8cc58350935712727be29990
DonatasNoreika <donoras@gmail.com>
2024-10-09 08:34:52 +0300

    + Admin puslapyje sukurti meniu punktus visiems sukurtiems modeliams

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (Car,
+                     CarModel,
+                     Service,
+                     Order,
+                     OrderLine)
 
 # Register your models here.
+admin.site.register(Car)
+admin.site.register(CarModel)
+admin.site.register(Service)
+admin.site.register(Order)
+admin.site.register(OrderLine)
