6e54c2f9bd0b9f70997f643de91480cd33a7050d
DonatasNoreika <donoras@gmail.com>
2024-10-10 08:04:13 +0300

    + 2: Padaryti, kad modelių pavadinimai būtų atvaizduojami teisingai (vienaskaita ir daugiskaita)

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -9,6 +9,10 @@ class Service(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Paslauga"
+        verbose_name_plural = "Paslaugos"
+
 
 class CarModel(models.Model):
     make = models.CharField(verbose_name="Gamintojas", max_length=50)
@@ -17,15 +21,24 @@ class CarModel(models.Model):
     def __str__(self):
         return f"{self.make} {self.model}"
 
+    class Meta:
+        verbose_name = "Modelis"
+        verbose_name_plural = "Modeliai"
+
 
 class Car(models.Model):
     plate = models.CharField(verbose_name="Valstybinis numeris", max_length=10)
     vin_code = models.CharField(verbose_name="VIN kodas", max_length=20)
     client_name = models.CharField(verbose_name="Klientas", max_length=50)
-    car_model = models.ForeignKey(to="CarModel", verbose_name="Modelis", on_delete=models.SET_NULL, null=True, blank=True)
+    car_model = models.ForeignKey(to="CarModel", verbose_name="Modelis", on_delete=models.SET_NULL, null=True,
+                                  blank=True)
 
     def __str__(self):
         return f"{self.plate} ({self.car_model})"
+
+    class Meta:
+        verbose_name = "Automobilis"
+        verbose_name_plural = "Automobiliai"
 
 
 class Order(models.Model):
@@ -35,6 +48,10 @@ class Order(models.Model):
     def __str__(self):
         return f"{self.date} - {self.car}"
 
+    class Meta:
+        verbose_name = "Užsakymas"
+        verbose_name_plural = "Užsakymai"
+
 
 class OrderLine(models.Model):
     order = models.ForeignKey(to="Order", verbose_name="Užsakymas", on_delete=models.CASCADE)
@@ -43,3 +60,7 @@ class OrderLine(models.Model):
 
     def __str__(self):
         return f"{self.service} - {self.quantity} ({self.order})"
+
+    class Meta:
+        verbose_name = "Eilutė"
+        verbose_name_plural = "Eilutės"
