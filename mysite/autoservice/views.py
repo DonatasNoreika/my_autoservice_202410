1df0b4ce2dff3757861d9e4a9ab8f085d103fe42
DonatasNoreika <donoras@gmail.com>
2024-10-15 08:07:02 +0300

    + 5: Padaryti, kad įrašai užsakymų puslapyje būtų puslapiuojami (per klasę).
    + 5: Padaryti, kad įrašai automobilių puslapyje būtų puslapiuojami (per funkciją).

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-
+from django.core.paginator import Paginator
 from .models import Service, Order, Car
 
 # Create your views here.
@@ -18,7 +18,11 @@ def index(request):
 
 
 def cars(request):
-    return render(request, template_name="cars.html", context={"cars": Car.objects.all()})
+    cars = Car.objects.all()
+    paginator = Paginator(cars, per_page=2)
+    page_number = request.GET.get("page")
+    paged_cars = paginator.get_page(page_number)
+    return render(request, template_name="cars.html", context={"cars": paged_cars})
 
 def car(request, car_id):
     return render(request, template_name="car.html", context={"car": Car.objects.get(pk=car_id)})
@@ -27,6 +31,7 @@ class OrderListView(ListView):
     model = Order
     template_name = "orders.html"
     context_object_name = "orders"
+    paginate_by = 4
 
 class OrderDetailView(DetailView):
     model = Order
