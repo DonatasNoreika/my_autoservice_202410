67d2ba54ba9b8a41d84fdc674a779067393b7cd1
DonatasNoreika <donoras@gmail.com>
2024-10-16 08:10:27 +0300

    + 6: Padaryti login puslapį, per kurį leistų prisijungti vartotojui.

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path("", views.index, name="index"),
     path("cars/", views.cars, name="cars"),
     path("cars/<int:car_id>", views.car, name="car"),
