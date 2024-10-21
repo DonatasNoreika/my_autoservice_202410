b5431522ca5f6238cb2e447e75418e02e9d20d0e
DonatasNoreika <donoras@gmail.com>
2024-10-21 08:10:25 +0300

    8: + Padaryti vartotojo registracijos formą pagal šioje pamokoje išmoktus žingsnius.

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path("register/", views.register, name="register"),
     path("", views.index, name="index"),
     path("cars/", views.cars, name="cars"),
     path("cars/<int:car_id>", views.car, name="car"),
